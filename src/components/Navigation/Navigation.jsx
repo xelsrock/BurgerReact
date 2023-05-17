fcb7018ab55a4a6910cc395587a9deb5a48f28f0
xelsrock <alex.perevosnicov@gmail.com>
2023-05-17 15:46:46 +0300

    API

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,7 +2,9 @@ import { useDispatch, useSelector } from "react-redux";
 import { Container } from "../Container/Container";
 import classes from './Navigation.module.css'
 import classNames from "classnames";
-import { changeCategory } from "../../store/category/categorySlice";
+import { categoryRequestAsync, changeCategory } from "../../store/category/categorySlice";
+import { useEffect } from "react";
+import { API_URL } from "../../const";
 
 
 export const Navigation = () => {
@@ -10,17 +12,21 @@ export const Navigation = () => {
   const { category, activeCategory } = useSelector((state) => state.category)
   const dispatch = useDispatch();
 
+  useEffect(() => {
+    dispatch(categoryRequestAsync())
+  }, [])
+
   return (
     <nav className={classes.navigation}>
       <Container className={classes.container}>
         <ul className={classes.list}>
           {category.map((item, index) =>
-            <li className={classes.item}>
+            <li className={classes.item} key={item.title}>
               <button
                 className={classNames(
                   classes.button,
                   activeCategory === index ? classes.button_active : '')}
-                style={{ backgroundImage: `url(${item.image})` }}
+                style={{ backgroundImage: `url(${API_URL}/${item.image})` }}
                 onClick={() => {
                   dispatch(changeCategory({indexCategory: index}))
                 }}
---
+++ src/const.js
@@ -0,0 +1,3 @@
+export const API_URL = 'http://localhost:3024';
+export const POSTFIX = '/api/product';
+
